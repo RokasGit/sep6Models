fb9c3efcabdaa387107e635a5dcbed01801295a0
Jakub_Platzek <jakub.platzek@gmail.com>
2023-04-22 18:52:37 +0200

    Adding of ID and toplist identification attributes to Movie

--- movie.ts
+++ movie.ts
@@ -1,9 +1,11 @@
 export interface Movie {
   // can add more properties here
+  ID: number;
   Title: string;
   Actors: string;
   Country: string;
   Released: string;
   Plot: string;
   Poster: string;
+  BelongsToToplist: boolean;
 }

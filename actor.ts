613ef6ba44838884556aec745cd53df9028cfb60
Mansory888 <nichita.railean@aol.com>
2023-05-06 13:01:00 +0200

    actor added

---
+++ actor.ts
@@ -0,0 +1,8 @@
+export interface Actor {
+    // can add more properties here
+    ID: number;
+    Name: string;
+    Popularity: number;
+    AverageRating: number;
+    known_for: any;   //its their movie
+}

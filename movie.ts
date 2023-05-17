0dbe2c4feb0cf78f28c170aac44376ac8bfac479
Rokas Paulauskas <ifamous.rp@gmail.com>
2023-05-17 21:04:01 +0200

    Movie mods

--- movie.ts
+++ movie.ts
@@ -2,17 +2,21 @@ export interface Movie {
   // can add more properties here
   imdbID: string;
   Title: string;
-  Actors: string;
-  Country: string;
+  Actors?: string;
+  Country?: string;
   Released: string;
-  Plot: string;
+  Plot?: string;
   Poster: string;
-  BelongsToToplist: boolean;
-  imdbRating: string;
-  imdbVotes: string;
-  Director: string;
-  Language: string;
-  Genre: string;
-  Year: string;
-  Type: string;
+  BelongsToToplist?: boolean;
+  imdbRating?: string;
+  imdbVotes?: string;
+  Director?: string;
+  Language?: string;
+  Genre?: string;
+  Year?: string;
+  Type?: string;
+}
+
+export interface Search {
+  Search: Movie[];
 }
